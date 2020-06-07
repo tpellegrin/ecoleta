68581ebf8359518dec0f753249f237f8c7467b28
tpellegrin <tgpellegrin@gmail.com>
2020-06-07 13:13:11 -0300

    Add Routing file, main pages routes mapping

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+
+import './styles.css';
+
+import logo from '../../assets/logo.svg';
 
 const CreatePoint = () => {
     return (
-        <h1>Hello World</h1>
+        <div id="page-create-point">
+            <header>
+                <img src={logo} alt="Ecoleta"/>
+
+                <Link to="/">
+                    <FiArrowLeft />
+                    Voltar para home
+                </Link>
+            </header>
+        </div>
     );
 }
 
